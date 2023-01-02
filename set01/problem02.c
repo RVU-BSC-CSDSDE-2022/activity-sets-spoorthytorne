b44067be7fc835d0de8631306f38077639bcb485
Spoorthy  T <114483219+spoorthytorne@users.noreply.github.com>
2023-01-02 05:14:02 +0000

    set02 p3

--- set01/problem02.c
+++ set01/problem02.c
@@ -4,6 +4,6 @@ int main (void) {
   int a, b,sum;
   a=2 ,b=4;
   sum=a+b;
-  printf("%d %d %d ",sum ,a ,b\n);
+  printf("%d %d %d ",a,b,sum);
   return 0;
 }
--- set02/problem03.c
+++ set02/problem03.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int input_number();
 int is_composite(int n);
 void output(int n, int result);
@@ -6,6 +7,10 @@ void output(int n, int result);
 int main(){
   int n,result;
   n=input_number();
+  if((n == 0)||(n == 1)){
+    printf("%d is neither prime nor composite\n", n);
+    exit(0);
+  }
   result=is_composite(n);
   output(n,result);
 }
@@ -19,9 +24,10 @@ int input_number(){
 
 int is_composite(int n){
   int i, count = 0;
- for(i= 1;i <n;i++) 
- if (n%i == 0)
+ for(i= 1;i <=n;i++) 
  {
+ if (n%i == 0)
+ 
    count = count+1; 
  }
    return count; 
@@ -29,8 +35,8 @@ int is_composite(int n){
 
   
  void output(int n,int result){
- if(result == 2)
-  printf("%d is a prime number",n); 
+ if(result >=2)
+  printf("%d is a not a composite number",n); 
  else
   printf("%d is a composite number",n); 
 } 
